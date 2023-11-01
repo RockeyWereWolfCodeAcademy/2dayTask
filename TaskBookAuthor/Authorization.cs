f8ec4cf4f06d6a7080d560f9c4ab1021d3233b93
RockeyWereWolf <mi71k9d7p@code.edu.az>
2023-11-01 11:38:50 +0400

    uploaded project

---
+++ TaskBookAuthor/Authorization.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Models;
+
+namespace TaskBookAuthor
+{
+    public static class Authorization
+    {
+        public static void Register(string name, string surname, string username, string password)
+        {
+            CheckExists:
+            //if (Program.Users.Find(user => user.Username.Contains(username)) != default)
+            //{
+            //    Console.WriteLine("User with this username is already exists, Try again?(Yes/Y):");
+            //    string response = Console.ReadLine().ToLower();
+            //    if (response == "y" || response == "yes")
+            //    {
+            //        username = Console.ReadLine();
+            //        goto CheckExists;
+            //    }
+            //    else
+            //    {
+            //        return;
+            //    }
+            //}
+            foreach (var user in Program.Users)
+            {
+                if (user.Username == username)
+                {
+                    Console.WriteLine("User with this username is already exists, Try again?(Yes/Y):");
+                    string response = Console.ReadLine().ToLower();
+                    if (response == "y" || response == "yes")
+                    {
+                        username = Console.ReadLine();
+                        goto CheckExists;
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+            }
+            CheckUsername:
+            if (username.Length <= 4)
+            {
+                Console.WriteLine("Username must be longer than 4 symbols, Try again?(Yes/Y):");
+                string response = Console.ReadLine().ToLower();
+                if (response == "y" || response == "yes")
+                {
+                    username = Console.ReadLine();
+                    goto CheckUsername;
+                }
+                else
+                {
+                    return;
+                }
+            }
+            CheckPassword:
+            if (password.Length <= 6)
+            {
+                Console.WriteLine("Password must be longer than 6 symbols, Try again(Yes/Y):");
+                string response = Console.ReadLine().ToLower();
+                if (response == "y" || response == "yes")
+                {
+                    username = Console.ReadLine();
+                    goto CheckPassword;
+                }
+                else
+                {
+                    return;
+                }
+            }
+            User newUser = new User()
+            {
+                Name = name,
+                Surname = surname,
+                Username = username,
+                Password = password
+            };
+            Program.Users.Add(newUser);
+        }
+
+        public static void Login(string username, string password)
+        {
+            CheckUsername:
+            foreach (var user in Program.Users)
+            {
+                if (user.Username != username)
+                {
+                    Console.WriteLine("User with this username does not exist, Try again?(Yes/Y):");
+                    string response = Console.ReadLine().ToLower();
+                    if (response == "y" || response == "yes")
+                    {
+                        username = Console.ReadLine();
+                        goto CheckUsername;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+        
+            //if (Program.Users.Find(user => user.Username.Contains(username)).Password == password)
+            //{
+            //    Console.WriteLine($"User {username} is logged in");
+            //    Program.LogedIn = Program.Users.Find(user => user.Username.Contains(username));
+            //    return;
+            //} else
+            //{
+            //    Console.WriteLine("Password is wrong. Try again(Yes/Y)");
+            //    string response = Console.ReadLine().ToLower();
+            //    if (response == "y" || response == "yes")
+            //    {
+            //        password = Console.ReadLine();
+            //        goto CheckPass;
+            //    }
+            //    else
+            //    {
+            //        return;
+            //    }
+            //}
+            foreach (var user in Program.Users)
+            {
+                if (user.Username == username)
+                {
+                CheckPass:
+                    if (user.Password == password)
+                    {
+                        Console.WriteLine("User is logged in");
+                        Program.LogedIn = user;
+                    } else
+                    {
+                        Console.WriteLine("Password is wrong. Try again(Yes/Y)");
+                        string response = Console.ReadLine().ToLower();
+                        if (response == "y" || response == "yes")
+                        {
+                            password = Console.ReadLine();
+                            goto CheckPass;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        public static void Logout ()
+        {
+            if (Program.LogedIn != null)
+            {
+                Console.WriteLine($"User {Program.LogedIn.Username} is loged out");
+                Program.LogedIn = null;
+            }
+            else
+            {
+                Console.WriteLine("User is not logged in");
+            }
+        }
+    }
+}
---
+++ TaskBookAuthor/Program.cs
@@ -0,0 +1,71 @@
+﻿using Core.Models;
+using System.Net.Sockets;
+
+namespace TaskBookAuthor
+{
+    internal class Program
+    {
+        public static List<User> Users { get; set; } = new List<User>();
+        public static User LogedIn { get; set; } = null;
+
+        public static void GetUsers()
+        {
+            foreach (var user in Users)
+            {
+                Console.WriteLine(user.Username);
+            }
+        }
+        static void Main(string[] args)
+        {
+            bool doLoop = true;
+            while (doLoop == true)
+            {
+                int option;
+                Console.WriteLine("What you want to do?\n1.Login/Logout(if user is logged in)\n2.Register\n3.Get all users\n4.Exit");
+                option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        if (LogedIn != null)
+                        {
+                            Console.WriteLine("User is logged in, logut(Yes/Y)?");
+                            string response = Console.ReadLine().ToLower();
+                            if (response == "yes" || response == "y")
+                            {
+                                Authorization.Logout();
+                            }
+                        } else
+                        {
+                            Console.Write("Enter username:");
+                            string usernameToLogin = Console.ReadLine();
+                            Console.Write("Enter password:");
+                            string passwordToLogin = Console.ReadLine();
+                            Authorization.Login(usernameToLogin, passwordToLogin);
+                        }
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter name:");
+                        string name = Console.ReadLine();
+                        Console.WriteLine("Enter surname:");
+                        string surname = Console.ReadLine();
+                        Console.WriteLine("Enter username:");
+                        string username = Console.ReadLine();
+                        Console.WriteLine("enter password:");
+                        string password = Console.ReadLine();
+                        Authorization.Register(name, surname, username, password);
+                        break;
+                    case 3:
+                        GetUsers();
+                        break;
+                    case 4:
+                        doLoop = false;
+                        break;
+                    default:
+                        Console.WriteLine("Wrong input!");
+                        break;
+
+                }
+            }
+        }
+    }
+}
